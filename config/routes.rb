3ce8d169fa13e5ae0806ee374a894198729853de
Drew Cosner <d.b.cosner@gmail.com>
2022-02-10 20:51:45 -0500

    added the NotoSansJP family of fonts and started using them

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'pages#home'
+  get 'home', to: 'pages#home'
+  get 'index', to: 'pages#home'
+
+  get 'appearance_testing', to: 'pages#appearance_testing'
 end
